5ab5252d1fb4ba39ee5c654495a27eb80cc805d1
Nikola Mihalek <nikola.mihalek@gmail.com>
2015-03-18 22:14:46 +0100

    initial commit

---
+++ gen/rs/ac/uns/ftn/xws/cbs/faktura/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.uns.ac.rs/xws/cbs/faktura", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package rs.ac.uns.ftn.xws.cbs.faktura;
